18882871ec0a9ba11677624e417625e16697510a
AleksVira <alekslkolr@gmail.com>
2019-07-01 22:51:24 +0300

    hometask_2 parseFullName

--- app/src/main/java/ru/virarnd/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/virarnd/devintensive/utils/Utils.kt
@@ -2,10 +2,11 @@ package ru.virarnd.devintensive.utils
 
 object Utils {
     fun parseFullName(fullName: String?): Pair<String?, String?> {
-        //TODO FIX ME!!!
         val parts: List<String>? = fullName?.split(" ")
-        val firstName = parts?.getOrNull(0)
-        val lastName = parts?.getOrNull(1)
+        val strFirst = parts?.getOrNull(0)
+        val strSecond = parts?.getOrNull(0)
+        val firstName = if (strFirst.isNullOrEmpty()) "null" else strFirst
+        val lastName = if (strSecond.isNullOrEmpty()) "null" else strSecond
         return firstName to lastName
     }
 
@@ -18,4 +19,3 @@ object Utils {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
-
